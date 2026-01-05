b6cff829929768e29eea12531befa56f4b58acc7
Gabriel <gabrielmacao07@gmail.com>
2026-01-05 18:44:29 -0300

    :broom: - cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-red-500">Só jogar o teste AQUI (pegar o app.tsx e colocar inteiro aqui!)</h1>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 gap-4">
+      <p className="text-4xl font-bold text-red-500">Só jogar o teste AQUI</p>
+      <p className="text-2xl font-bold text-blue-500">Pega o app.tsx e coloca inteiro aqui!</p>
     </div>
   );
 }
